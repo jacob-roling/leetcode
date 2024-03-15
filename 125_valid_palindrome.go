627197c0d83b2a3900d7cd4acddf6539d65d83c7
Jacob, Roling <jacob.roling@hey.com>
2024-03-15 12:53:42 +1030

    chore: savepoint

--- 125_valid_palindrome.go
+++ 125_valid_palindrome.go
@@ -2,21 +2,21 @@ package leetcode
 
 import (
 	"strings"
+	"unicode"
 )
 
 func ValidPalindrome(s string) bool {
-	var alphanum = "abcdefghijklmnopqrstuvwxyz0123456789"
-	s = strings.ToLower(s)
-	var formatted = make([]byte, 0)
-
-	for i := 0; i < len(s); i++ {
-		if strings.ContainsRune(alphanum, rune(s[i])) {
-			formatted = append(formatted, s[i])
+	filter := func(r rune) rune {
+		if !unicode.IsLetter(r) && !unicode.IsNumber(r) {
+			return -1
 		}
+		return unicode.ToLower(r)
 	}
 
-	for i, j := 0, len(formatted)-1; i < j; {
-		if formatted[i] != formatted[j] {
+	s = strings.Map(filter, s)
+
+	for i, j := 0, len(s)-1; i < j; {
+		if s[i] != s[j] {
 			return false
 		}
 		i++
@@ -25,3 +25,14 @@ func ValidPalindrome(s string) bool {
 
 	return true
 }
+
+// First method of formatting
+// var alphanum = "abcdefghijklmnopqrstuvwxyz0123456789"
+// s = strings.ToLower(s)
+// var formatted = make([]byte, 0)
+
+// for i := 0; i < len(s); i++ {
+// 	if strings.ContainsRune(alphanum, rune(s[i])) {
+// 		formatted = append(formatted, s[i])
+// 	}
+// }
